480fad3851f2ee8a2d6d77a9e0ece604adaf3989
emlynjones <emlynjones121@gmail.com>
2015-07-02 18:47:58 +0100

    Fersiwn 1.0
    Plaen - dim newidiadau dim ond manylion GCW yn y style.css.

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * thema Theme Customizer
+ *
+ * @package thema
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function thema_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'thema_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function thema_customize_preview_js() {
+	wp_enqueue_script( 'thema_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'thema_customize_preview_js' );
