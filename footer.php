586dacd5af02b79698a657790e129c43e5cc3a78
emlynjones <emlynjones121@gmail.com>
2015-07-05 21:24:31 +0100

    CSS & Footer
    Wedi newid CSS er mwyn iddo edrych yn fwy fel Thema.
    
    Wedi newid geiriad Footer.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *
  * Contains the closing of the #content div and all content after
  *
- * @package thema
+ * @package Thema
  */
 
 ?>
@@ -15,7 +15,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'thema' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'thema' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'thema' ), 'thema', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( '%1$s by %2$s.', 'thema' ), 'thema', '<a href="http://thema.cymru/" rel="designer">Thema</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
