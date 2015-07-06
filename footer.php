05c0727d607f74f4674a6dc5f56ccb1fc3bf4b92
emlynjones <emlynjones121@gmail.com>
2015-07-06 18:14:17 +0100

    Footer CSS - Thema
    Steilio footer i ddangos jest "Thema" a cadw'r brandio yno ar waelod y
    footer.

--- footer.php
+++ footer.php
@@ -13,9 +13,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'thema' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'thema' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( '%1$s by %2$s.', 'thema' ), 'thema', '<a href="http://thema.cymru/" rel="designer">Thema</a>' ); ?>
+
+			<h2><a href="http://thema.cymru/" rel="designer">Thema.</a></h2>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
