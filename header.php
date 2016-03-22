6c183fdbdcde5f9ec90a00e2b373db0a0a34e1ac
emlynjones <emlynjones121@gmail.com>
2016-03-22 21:10:35 +0000

    Steilio a newid pennyn
    Adio target = "_blank" ar gyfer yr eiconnau social & tynnu hardcode
    max-width o'r header.php. Steilio ymatebol i fod yn fwy crand.

--- header.php
+++ header.php
@@ -28,39 +28,41 @@
 			$thema_logo = get_theme_mod('thema-logo-gwefan');
 			?>
 
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class = "thema-logo" style = "max-width:200px; float:left;"src = "<?php echo $thema_logo; ?>"/><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class = "thema-logo" src = "<?php echo $thema_logo; ?>"/><?php bloginfo( 'name' ); ?></a></h1>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
-		<div class = "thema-language-switch">
-			<?php
-				if(function_exists('pll_the_languages')){
-					$args = array(
-						'show_names' => 1,
-						'hide_current' => false
-					);
-
-					pll_the_languages($args);
-
-				}
-			 ?>
-		</div>
-		<?php
-			$thema_url_twitter = get_theme_mod('thema-url-twitter');
-			$thema_url_facebook = get_theme_mod('thema-url-facebook');
-		 ?>
-		<div class = "cyfryngau-cymdeithasol">
-
-			<ul class = "thema-eiconnau-rhestr">
+			<div class = "thema-language-switch">
 				<?php
-				//Falle angen bod yn fwy picky yma? Os nad yw'r darn ar ol y url wedi ei lenwi?
-				if ($thema_url_twitter != "http://twitter.com/"){ ?>
-				<li><a class="twitter" href="<?php echo $thema_url_twitter; ?>"><i class="fa fa-twitter"></i></a></li>
-				<?php } ?>
-				<?php if ($thema_url_facebook != "http://facebook.com/"){ ?>
-				<li><a class="facebook" href="<?php echo $thema_url_facebook; ?>"><i class="fa fa-facebook-square"></i></a></li>
-				<?php } ?>
-			</ul>
-		</div>
+					if(function_exists('pll_the_languages')){
+						$args = array(
+							'show_names' => 1,
+							'hide_current' => false
+						);
+
+						pll_the_languages($args);
+
+					}
+				 ?>
+			</div>
+			<?php
+				$thema_url_twitter = get_theme_mod('thema-url-twitter');
+				$thema_url_facebook = get_theme_mod('thema-url-facebook');
+			 ?>
+			<div class = "cyfryngau-cymdeithasol">
+
+				<ul class = "thema-eiconnau-rhestr">
+					<?php
+					//Falle angen bod yn fwy picky yma? Os nad yw'r darn ar ol y url wedi ei lenwi?
+					if ($thema_url_twitter != "http://twitter.com/"){ ?>
+					<li><a class="twitter" href="<?php echo $thema_url_twitter; ?>"target = "_BLANK"><i class="fa fa-twitter"></i></a></li>
+					<?php } ?>
+					<?php if ($thema_url_facebook != "http://facebook.com/"){ ?>
+					<li><a class="facebook" href="<?php echo $thema_url_facebook; ?>" target = "_BLANK"><i class="fa fa-facebook-square"></i></a></li>
+					<?php } ?>
+				</ul>
+			</div>
+		</div><!-- .site-branding -->
+
+
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Dewislen', 'thema' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
